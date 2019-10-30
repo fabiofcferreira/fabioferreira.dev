7a9356477cd091f4eb3faf5db33c0289f658af02
Fábio Ferreira <ffcfpten@gmail.com>
2019-10-30 18:24:26 +0000

    feat: add automatically updated age with moment

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 particlesJS.load('particles', 'particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  let bio = document.querySelector('div#bio')
+  let age = moment().diff(moment("2001-07-18"), 'years')
+
+  bio.innerText = `${age}y old self-taught full stack developer`
+})
